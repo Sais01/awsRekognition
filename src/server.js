c38ca7434ff1da1c1493e12137fd5f96c36a4b2a
Crisofer Gaier Sais <cristofer.gaier.sais@gmail.com>
2023-09-22 11:19:47 -0300

    :sparkles: feat:Adicionando funcionalidade de detectLabels

--- src/server.js
+++ src/server.js
@@ -16,6 +16,9 @@ app.use(bodyParser.urlencoded({
 
 const aws = require('aws-sdk');
 
+const multer = require('multer');
+const upload = multer();
+
 // we've started you off with Express,
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
@@ -27,7 +30,60 @@ app.get("/", function(request, response) {
   response.sendFile(__dirname + "/views/index.html");
 });
 
+app.post("/detectarLabels", upload.single('myfile'),function(request, response) {
+  aws.config.update({
+    region: process.env.AWS_REGION
+  });
+  
+  var rekognition = new aws.Rekognition(); 
+
+  var arquivo = request.file.buffer;
+  var params = {
+    Image: {
+      Bytes: arquivo
+    },
+    MaxLabels: 50,
+    MinConfidence: 70
+  };
+
+  rekognition.detectLabels(params, function(err, data) {
+    if(err){
+      console.log(err, err.stack);
+    }else{
+      console.log(data);
+      
+      var table = "<table border=1>";
+      table += "<tr><th>Nome</th><th>Confiança</th></tr>";
+      for(var i = 0; i < data.Labels.length; i++){
+        table += "<tr><td>" + data.Labels[i].Name + "</td><td>" + data.Labels[i].Confidence + "</td></tr>";
+      }
+      table+= "</table>";
+
+      response.send(table);
+
+    }
+  });
+
+});
+
 // listen for requests :)
 const listener = app.listen(process.env.PORT, function() {
   console.log("Your app is listening on port " + listener.address().port);
 });
+
+// var rekognition = new aws.Rekognition(); 
+
+// var params = {
+//   Image: {
+//     Bytes: new Buffer(request.body.image, 'base64')
+//   },
+//   MaxLabels: 10,
+//   MinConfidence: 70
+// };
+
+// rekognition.detectLabels(params, function(err, data) {
+//   if(err){
+//     console.log(err, err.stack);
+//   }else{
+//     console.log(data);
+//   }
